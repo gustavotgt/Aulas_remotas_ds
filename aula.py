b05eaedbbcb401c26ddd58407cc6984c4a83b905
DR.Perus <71403799+gustavotgt@users.noreply.github.com>
2025-11-06 14:45:25 -0300

    feat: add code aula.py

--- aula.py
+++ aula.py
@@ -1,1 +1,20 @@
-print("ola mundo")
+#exercicio 1. verificaçao de idade
+
+ # Nome do arquivo: atividade2.py
+
+# 1. Pergunte o nome e guarde na variável 'nome'
+nome = input("Qual é o seu nome? ")
+
+# 2. Pergunte a idade e guarde na variável 'idade_texto'
+idade_texto = input("Qual é a sua idade? ")
+
+# 3. Converta a idade de texto para número
+idade_numero = int(idade_texto)
+
+# 4. Crie a lógica condicional
+if idade_numero >= 18:
+    # Complete com a mensagem para maior de idade
+    print(f"Olá, {nome}, você é maior de idade.")
+else:
+    # Complete com a mensagem para menor de idade
+    print(f"Olá, {nome}, você é menor de idade.")
