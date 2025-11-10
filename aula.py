2bb2533dbe82c7129b4aa49f947de234d12d04c2
DR.Perus <71403799+gustavotgt@users.noreply.github.com>
2025-11-09 23:18:09 -0300

    feata: add

--- aula.py
+++ aula.py
@@ -1,35 +1,20 @@
-#exercicio 1. verificaçao de idade
+nome = input("Qual é o seu nome? ")
 
+idade_texto = input("Qual é a sua idade? ")
 
-Nome = input("Qual é o seu nome? ")
+idade = int(idade_texto)
 
-
-idade_txt = input("Qual é a sua idade? ")
-
-
-idade_num = int(idade_txt)
-
-
-if idade_num >= 18:
-  
-    print(f"Olá, {Nome}, você é maior de idade.")
-else:
-    
-    print(f"Olá, {Nome}, você é menor de idade.")
-
-
-    #excercicio 2. tabuada
-
-   
+    if idade >= 18:
+        print(f"Olá, {nome}, você é maior de idade.")
+    else:
+        print(f"Olá, {nome}, você é menor de idade.")
+dawdawd
+22
 numero_str = input("Digite um número para ver a tabuada: ")
 numero = int(numero_str)
 
-print(f"--- Tabuada do {numero} ---")
+    print(f"\n--- Tabuada do {numero} ---")
 
-for i in range(1, 11):
-    
-    
-    resultado = numero * i
-    
-   
-    print(f"{numero} x {i} = {resultado}")
+    for i in range(1, 11):
+        resultado = numero * i
+        print(f"{numero} x {i} = {resultado}")
